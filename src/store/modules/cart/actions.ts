44caa7527ee31a1a90e9cb6442f88d78e5e8fcdf
Matheus Henrique <sethwololo@gmail.com>
2021-01-27 14:56:23 -0300

    Tipagens adicionadas a state e Cart

--- src/store/modules/cart/actions.ts
+++ src/store/modules/cart/actions.ts
@@ -1,7 +1,5 @@
 import { IProduct } from './types';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const addProductToCart = (product: IProduct) => {
   return {
     type: 'ADD_PRODUCT_TO_CART',
